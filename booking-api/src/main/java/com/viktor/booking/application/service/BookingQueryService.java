7826e7c65c4e68845c3da982219bb04cac4b2a42
Viktor Schlenkin <193060857+schloenkin@users.noreply.github.com>
2026-06-29 11:54:06 +0200

    Store bookings in memory

--- booking-api/src/main/java/com/viktor/booking/application/service/BookingQueryService.java
+++ booking-api/src/main/java/com/viktor/booking/application/service/BookingQueryService.java
@@ -5,50 +5,60 @@ import com.viktor.booking.domain.model.Booking;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 @Service
 public class BookingQueryService {
 
-    public List<Booking> getAllBookings() {
-        Booking booking1 = new Booking(
+    private final List<Booking> bookings = new ArrayList<>();
+    private Long nextId = 3L;
+
+    public BookingQueryService() {
+        bookings.add(new Booking(
                 1L,
                 1L,
                 1L,
                 LocalDateTime.of(2026, 6, 25, 10, 0),
                 LocalDateTime.of(2026, 6, 25, 10, 30),
                 BookingStatus.CONFIRMED
-        );
+        ));
 
-
-        Booking booking2 = new Booking(
+        bookings.add(new Booking(
                 2L,
                 2L,
                 2L,
                 LocalDateTime.of(2026, 6, 25, 11, 0),
                 LocalDateTime.of(2026, 6, 25, 12, 0),
                 BookingStatus.PENDING
-        );
+        ));
+    }
 
-        return List.of(booking1, booking2);
+    public List<Booking> getAllBookings() {
+        return bookings;
     }
 
     public Optional<Booking> getBookingById(Long id) {
-        return getAllBookings()
+        return bookings
                 .stream()
                 .filter(booking -> booking.getId().equals(id))
                 .findFirst();
     }
+
     public Booking createBooking(Long userId, Long serviceId, LocalDateTime startTime, LocalDateTime endTime) {
-        return new Booking(
-                3L,
+        Booking booking = new Booking(
+                nextId,
                 userId,
                 serviceId,
                 startTime,
                 endTime,
                 BookingStatus.PENDING
         );
-    }
 
+        bookings.add(booking);
+        nextId++;
+
+        return booking;
+    }
 }
