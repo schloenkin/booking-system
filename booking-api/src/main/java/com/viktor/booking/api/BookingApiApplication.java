dbf08395482041dad7f9c613448afe706826b503
Viktor Schlenkin <193060857+schloenkin@users.noreply.github.com>
2026-07-22 13:19:22 +0200

    Activate JPA persistence

--- booking-api/src/main/java/com/viktor/booking/api/BookingApiApplication.java
+++ booking-api/src/main/java/com/viktor/booking/api/BookingApiApplication.java
@@ -1,9 +1,18 @@
 package com.viktor.booking.api;
 
+import com.viktor.booking.infrastructure.persistence.entity.BookingEntity;
+import com.viktor.booking.infrastructure.persistence.repository.BookingJpaRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 
 @SpringBootApplication(scanBasePackages = "com.viktor.booking")
+@EntityScan(basePackageClasses = BookingEntity.class)
+@EnableJpaRepositories(
+        basePackageClasses = BookingJpaRepository.class
+)
 public class BookingApiApplication {
 
     public static void main(String[] args) {
